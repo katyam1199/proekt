33b93504cfcbcdad25b7b1201a4b12c2493c6104
katyam1199 <167461504+katyam1199@users.noreply.github.com>
2024-04-26 18:21:18 +0300

    Add files via upload

---
+++ ConsoleApplication20.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <list>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    list<int> l = {};
+    int n, num;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> num;
+        l.push_back(num);
+    }
+    auto min_it = (min_element(l.begin(), l.end()));
+    int min = *min_it;
+    int amount = count(l.begin(), l.end(), min);
+    if (amount != 2)
+    {
+        cout << 0;
+        return 0;
+    }
+    else
+    {
+        amount = 0;
+        auto sub_it = min_it;
+        for (auto it = ++sub_it; *it != *min_it; it++)
+        {
+            amount++;
+        }
+        cout << amount;
+        return 0;
+    }
+}
